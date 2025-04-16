3d34e1d5787633c5d04891dbbe4744847eaf1449
Kailin Zhang <k399zhan@uwaterloo.ca>
2025-04-15 20:01:39 -0400

    Initial commit

---
+++ build/CMakeFiles/tracker.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tracker.dir/tracker.cc.o"
+  "CMakeFiles/tracker.dir/tracker.cc.o.d"
+  "tracker"
+  "tracker.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tracker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
